7fc8a1ffde16b826a899c58ab06371e319d20082
Zach Nicoll <zacho7@hotmail.com.au>
2021-08-28 15:43:30 +1000

    Slash Command support
    Slash Command support and more environment variables

--- bot.py
+++ bot.py
@@ -5,16 +5,20 @@ from discord.errors import Forbidden
 from discord.ext import commands
 from discord.ext.commands.converter import Greedy
 from discord.ext.commands.core import has_permissions
-from discord.member import Member
+from discord.guild import Guild
+from discord.user import User
 from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_option
+from discord_slash.utils.manage_commands import create_choice, create_option
 from dotenv import load_dotenv
 
 #TODO: need to get cogs working properly
-#TODO: slash commands
 
 client = commands.Bot(command_prefix = '.')
-slash = SlashCommand(client)
+slash = SlashCommand(client, sync_commands=True)
+
+load_dotenv()
+
+guild_ids= [int(os.getenv('TEST_SERVER')), int(os.getenv('R34_SERVER'))]
 
 @client.event
 async def on_ready():
@@ -28,7 +32,9 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-@slash.slash(name='ror', description="Gernerates a Random ROR2 character to play with random artifacts")
+# random commands
+
+@slash.slash(name='ror', description="Gernerates a Random ROR2 character to play with random artifacts", guild_ids= guild_ids)
 @client.command(name="ror")
 async def ror(ctx):
     characters = ["Commando", "Huntress", 'MUL-T', 'Engineer', 'Mercenary', 'REX', 'Loader', 'Acrid', 'Artificer', 'Capitan']
@@ -41,30 +47,51 @@ async def ror(ctx):
     
     await ctx.send(f'Survivor: {random.choice(characters)}\nArtifact(s): {artifact}')
 
-# @slash.slash(name='ShuffleUser', description="shuffle user between two channels", 
-# options=[create_option(name='targets', description='user(s) to shuffle', option_type=6, required=True)])
+@slash.slash(name='PSR', description='Play paper sissors rock against the bot', guild_ids= guild_ids,
+options=[create_option(name='choice',
+                        description='select your weapon',
+                        option_type=3,
+                        required=True,
+                        choices=[
+                            create_choice(name='paper', value='paper'),
+                            create_choice(name='sissors', value='sissors'),
+                            create_choice(name='rock', value='rock')
+                        ])])
+@client.command(name='PSR')
+async def psr(ctx, choice: str):
+    options = ['paper', 'sissors', 'rock']
+    player = choice.lower()
+    bot = random.choice(options)
+    if player == bot:
+        await ctx.send(f'Bot chose:{bot}\nDraw!')
+    elif (player == 'paper' and bot == 'rock') or (player == 'sissors' and bot == 'paper') or (player == 'rock' and bot == 'sissors'):
+        await ctx.send(f'Bot chose:{bot}\nYou Win!')
+    else:
+        await ctx.send(f'Bot chose:{bot}\nYou Lose!')
+
+
+# Admin commands
+
+@slash.slash(name='ShuffleUser', description="shuffle user between two channels", guild_ids= guild_ids,
+options=[create_option(name='target', description='user(s) to shuffle', option_type=6, required=True)])
 @client.command(aliases=["su"])
 @has_permissions(administrator=True)
-async def shuffleUser(ctx, targets: Greedy[Member]):
-    channel1 = client.get_channel(803786402947923998)
- 
-    if not len(targets):
-        await ctx.send("no members selected")
+async def shuffleUser(ctx, target: User):
+    channel1 = client.get_channel(int(os.getenv('SHUFFLE_CHANNEL')))
+    if not target:
+        await ctx.send("no member selected")
     else:
-        for target in targets:
-            channel2 = target.voice.channel
-            i = 0
-            while i < 2:
-                await target.move_to(channel1)
-                await target.move_to(channel2)
-                i = i + 1
-            await ctx.send(f'member shuffled: {target.name}')  
+        channel2 = target.voice.channel
+        i = 0
+        while i < 2:
+            await target.move_to(channel1)
+            await target.move_to(channel2)
+            i = i + 1 
+            await ctx.send(f'member shuffled: {target.name}')
 
 @shuffleUser.error
 async def shuffle_user_error(ctx, exc):
     if isinstance(exc, Forbidden):
         await ctx.send("You don't have the correct permissons to use this command") 
-
-load_dotenv()
 
 client.run(os.getenv('TOKEN'))
